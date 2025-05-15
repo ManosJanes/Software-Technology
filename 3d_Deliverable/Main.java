3a0fa23a1155288c5fc724f3120a37a507ebc8c8
ManosJanes <150934462+ManosJanes@users.noreply.github.com>
2025-05-15 19:51:08 +0300

    Delete 3d_Deliverable/Main.java

--- 3d_Deliverable/Main.java
+++
@@ -1,8 +0,0 @@
-import model.User;
-
-public class Main {
-    public static void main(String[] args) {
-        User user = new User();
-        System.out.println("Created a new User object: " + user);
-    }
-}
