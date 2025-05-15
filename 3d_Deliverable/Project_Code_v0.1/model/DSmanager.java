7f1cf2b75f21d3b92123a36f6e3eeb54e6923d22
ManosJanes <150934462+ManosJanes@users.noreply.github.com>
2025-05-15 20:19:31 +0300

    Add files via upload

---
+++ 3d_Deliverable/Project_Code_v0.1/model/DSmanager.java
@@ -0,0 +1,8 @@
+package model;
+
+public class DSmanager {
+
+    public DSmanager() {
+        // Default constructor
+    }
+}
---
+++ 3d_Deliverable/Project_Code_v0.1/model/Employees.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Employees {
+
+    public Employees() {
+        // Default constructor
+    }
+}
